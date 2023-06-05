08c33e091c8ce8aa85283688f39d3f7a4c092dc2
ddixit14 <deepankardixit@google.com>
2023-06-05 11:17:51 -0400

    updating script to handle python's readme.rst files

--- repo_status.sh
+++ repo_status.sh
@@ -14,16 +14,44 @@ repositories=$language.txt
 
 # Define the column headers
 echo "# $language" > $language-results.md
-echo "| Repository | Open Issues | Open Pull Requests |" >> $language-results.md
-echo "|------------|-------------|--------------------|" >> $language-results.md
+echo "| Repository | Open Issues | Open Pull Requests | README.MD updated | About updated | Public Archived |" >> $language-results.md
+echo "|------------|-------------|--------------------|--------------------|--------------------|--------------------|" >> $language-results.md
 
 for repository in $(cat $repositories); do
 
   open_issues_count=$(gh issue list -R googleapis/${repository} -L 100 -s open | wc -l)
   open_pull_requests=$(gh pr list -R googleapis/${repository} -L 100 -s open | wc -l)
 
-  echo "| $repository | $open_issues_count | $open_pull_requests |" >> $language-results.md
+  if [[ $language == "python" ]]; then
+      output_readme=$(curl -s https://raw.githubusercontent.com/googleapis/${repository}/main/README.rst)
+      if [[ $output_readme == *"This github repository is archived"* ]]; then
+         is_present_readme=true
+      else
+         is_present_readme=false
+      fi
+  else
+      output_readme=$(curl -s https://raw.githubusercontent.com/googleapis/${repository}/main/README.md)
+      if [[ $output_readme == *"library has moved to"* ]]; then
+        is_present_readme=true
+      else
+        is_present_readme=false
+      fi
+  fi
+
+  output_about=$(gh repo view googleapis/${repository} --json description -q '.description')
+  if [[ $output_about == *"library has moved"* ]]; then
+      is_present_about=true
+  else
+      is_present_about=false
+  fi
+
+  status=$(gh repo view googleapis/${repository} --json isArchived -q '.isArchived')
+  if [[ $status == "true" ]]; then
+      is_public_archive=true
+  else
+      is_public_archive=false
+  fi
+
+  echo "| $repository | $open_issues_count | $open_pull_requests | $is_present_readme | $is_present_about | $is_public_archive |" >> $language-results.md
 
 done
-
-
