647752ec491a6ec6a18b2dad54bf3c5d80407729
Deepankar Dixit <90280028+ddixit14@users.noreply.github.com>
2023-07-20 17:30:51 -0400

    Update file_existence.sh

--- file_existence.sh
+++ file_existence.sh
@@ -26,14 +26,14 @@ fi
 
 # Read the contents of the first file
 contents_1=$(cat $file_1)
-echo "files in repo are:" $contents_1
+# echo "files in repo are:" $contents_1
 rm files_in_repo.txt
 # Read the contents of the second file
 contents_2=$(cat $file_2 |sort -d)
 #echo "files that should exist:" $contents_2
 # Check if the contents of the two files are equal
-#if [[ "$contents_1" == "$contents_2" ]]; then
-#  echo "true"
-#else
-#  echo "false"
-#fi
+if [[ "$contents_1" == "$contents_2" ]]; then
+ echo "true"
+else
+ echo "false"
+fi
